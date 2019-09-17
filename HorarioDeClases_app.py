42a37ca7e1f1e0b742ba91ab6d1bef3b17554572
simonsanvil <simonsanvil2012@gmail.com>
2019-09-17 20:32:07 +0200

    first commit

---
+++ HorarioDeClases_app.py
@@ -0,0 +1,54 @@
+from requests import Session
+import urllib
+from lxml import html
+import os
+
+#UC3M url that links to the calendars webpage:
+URL = "https://aplicaciones.uc3m.es/horarios-web/alumno/alumno.page"
+#Aula Global Credentials:
+if not os.path.isfile("AulaCredentials.txt"):
+    print('Introduce tus credenciales de Aula Global: [NIA (100XXXXXX) y contraseña]')
+    NIA = input('NIA/Numero de Estudiante/Student Number: ') # ## TODO: NUMERO DE ESTUDIANTE / USUARIO DE AULA GLOBAL
+    PASS = input('Contraseña/Password: ')# #TODO
+    with open("AulaCredentials.txt") as f:
+        f.write(NIA + "\n")
+        f.write(PASS + "\n")
+else:
+    with open("AulaCredentials.txt") as f:
+        lineList = [line.rstrip('\n') for line in open("AulaCredentials.txt")]
+        NIA = lineList[0]
+        PASS = lineList[1]
+
+#Scraping the .ics file to get the calendars:
+with Session() as s:
+
+    print('Scraping ' + URL + ' to get .ics file')
+    print("Attempting to log into Aula Global")
+    ##To login to Aula global using the credentials above
+    site = s.get("https://login.uc3m.es/index.php/CAS/login?service=https%3A%2F%2Faulaglobal.uc3m.es%2Flogin%2Findex.php&gateway=true")
+    login_data = {"adAS_i18n_theme": "es",
+                  "adAS_mode":"authn",
+                  "adAS_username":NIA,
+                  "adAS_password":PASS}
+    s.post("https://login.uc3m.es/index.php/CAS/login?service=https%3A%2F%2Faulaglobal.uc3m.es%2Flogin%2Findex.php&gateway=true",login_data)
+    print('Log in was succesful')
+    ##To enter the horarios webpage and access all the references with links
+    calendar_page = s.get(URL)
+    webpage = html.fromstring(calendar_page.content)
+    hrefList = webpage.xpath('//a/@href')
+    ##To download the ics files and put them into the "horarios" folder:
+    ## HORARIO CORRESPONDIENTE AL PRIMER CUATRIMESTRE:
+    calendarLink_1 = "https://aplicaciones.uc3m.es" + hrefList[2]
+    cal_q1 = s.get(calendarLink_1)
+    calFile_1 = str(os.path.dirname(os.path.realpath(__file__))) + "/horarios/horario_Q1.ics"
+    horarioCuatri_1 = open(calFile_1, 'wb').write(cal_q1.content)
+    print('Horario 1er Cuat. obtained')
+
+    ## HORARIO CORRESPONDIENTE AL SEGUNDO CUATRIMESTRE:
+    calendarLink_2 = "https://aplicaciones.uc3m.es" + hrefList[4]
+    cal_q2 = s.get(calendarLink_2)
+    calFile_2 = str(os.path.dirname(os.path.realpath(__file__))) + "/horarios/horario_Q2.ics"
+    horarioCuatri_2 = open(calFile_2, 'wb').write(cal_q2.content)
+    print('Horario 2do Cuat. obtained')
+
+import ParsingAndImport_cal
---
+++ ParsingAndImport_cal.py
@@ -0,0 +1,174 @@
+##GOOGLE
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+import vobject
+import csv
+import os
+import sys
+
+
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+def parseICS():
+
+    print('Parsing .ics files to csv')
+    with open('events.csv', mode='w') as csv_out:
+        csv_writer = csv.writer(csv_out, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow(['WHAT', 'WHERE', 'FROM', 'TO', 'DESCRIPTION','CATEGORY'])
+
+        # read the data from the file
+        dataQ1 = open("horarios/horario_Q1.ics").read()
+        dataQ2 = open("horarios/horario_Q2.ics").read()
+
+        # iterate through the contents of the .ics corresponding to the calendar of the first semester
+        for cal in vobject.readComponents(dataQ1):
+            for component in cal.components():
+                if component.name == "VEVENT":
+                    a = 2
+                    # # write to csv
+                    csv_writer.writerow([restoreAccents(component.summary.valueRepr()) ,
+                                        restoreAccents(component.location.valueRepr()),
+                                        component.dtstart.valueRepr(),
+                                        component.dtend.valueRepr(),
+                                        restoreAccents(component.description.valueRepr()),
+                                        component.categories.valueRepr()])
+        print('Q1 .ics succesfuly parsed')
+        # iterate through the contents of the .ics corresponding to the calendar of the second semeste
+        for cal in vobject.readComponents(dataQ2):
+            for component in cal.components():
+                if component.name == "VEVENT":
+                    a = 2
+                    # # write to csv
+                    csv_writer.writerow([restoreAccents(component.summary.valueRepr()) ,
+                                        restoreAccents(component.location.valueRepr()),
+                                        component.dtstart.valueRepr(),
+                                        component.dtend.valueRepr(),
+                                        restoreAccents(component.description.valueRepr()),
+                                        component.categories.valueRepr()])
+        print('Q2 .ics succesfuly parsed')
+
+#Google Calendar API Integration:
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+def importToGoogleCalendar():
+    """
+    Creates a new "Horario de clases" calendar and imports all the events from the calendar parsed to a csv file in to it.
+    """
+    print('Importing parsed events to google calendar. Google authentication may be required')
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    # Call the Calendar API
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+
+    #GET A LIST OF ALL THE CALENDARS
+    calendar_list = service.calendarList().list().execute()
+    myCalendars = {}
+    for i in range(0,len(calendar_list['items'])):
+        myCalendars[calendar_list['items'][i]['summary']] = calendar_list['items'][i]['id']
+
+    # CREATE NEW CALENDAR AND IMPORT ALL THE CLASS EVENTS THERE
+    newCalendarName = 'Horario de Clases ' + getYearInterval()
+    if newCalendarName in myCalendars:
+        #DELETE 'HORARIO DE CLASES' CALENDAR IF ANOTHER EXISTS ALREADY
+        service.calendars().delete(calendarId= myCalendars[newCalendarName]).execute()
+        print('Previous calendar ("'+ newCalendarName + ')" deleted')
+
+    print('Creating new "Horario de Clases" calendar')
+    calendar = {
+            'summary': newCalendarName,
+            'description':'Este calendario fue creado usando HorarioDeClases_app disponible en github.com/simonsanvil',
+            'timeZone': 'Europe/Madrid'
+    }
+    new_calendar = service.calendars().insert(body=calendar).execute()
+    print("calendar '" + newCalendarName + "' created")
+    #IMPORT ALL THE EVENTS FROM CSV TO GOOGLE CALENDAR...
+    print('Importing events to new calendar. This might take a while...')
+    eventList = getEventListFromCSV()
+    for event in eventList:
+        startTime = d = datetime.datetime.strptime(event[2],'%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%dT%H:%M:%S+02:00')
+        endTime = d = datetime.datetime.strptime(event[3],'%Y-%m-%d %H:%M:%S').strftime('%Y-%m-%dT%H:%M:%S+02:00')
+        CalendarEvent = {
+              'summary': event[0],
+              'location': event[1],
+              'description': event[4],
+              'start': {
+                'dateTime': startTime,
+                'timeZone': 'Europe/Madrid',
+              },
+              'end': {
+                'dateTime': endTime,
+                'timeZone': 'Europe/Madrid',
+              },
+              'reminders': {
+                'useDefault': False,
+                'overrides': [
+                  {'method': 'popup', 'minutes': 20},
+                  ],
+              },
+              'colorId':'4',
+        }
+        newEvent = service.events().insert(calendarId=new_calendar['id'], body=CalendarEvent).execute()
+        print('Event created: %s %s' %(newEvent['summary'],newEvent['start']))
+    print('All events have been imported')
+
+def getEventListFromCSV():
+    """
+    Creates a python list containing each event row from the events csv as it's elements
+    """
+    with open('events.csv', newline = '') as f:
+        csv_reader = csv.reader(f)
+        try:
+            eventList = []
+            for row in csv_reader:
+                eventList.append(row)
+            eventList = list(filter(lambda a: a != [] ,eventList))
+            eventList.pop(0)
+            return eventList
+        except csv.Error as e:
+            sys.exit('file {}, line {}: {}'.format(filename, reader.line_num, e))
+            return None
+
+def restoreAccents(s):
+    s = s.replace('Ã¡','á')
+    s = s.replace('Ã³','ó')
+    s = s.replace('Ã©','é')
+
+    s = s.replace('Ã±','ñ')
+    s = s.replace('Ã­','í')
+
+    return s
+
+def getYearInterval():
+    now = datetime.datetime.now()
+    if now.month < 9:
+        YearInterval = str(now.year-1) + '/' + str(now.year)
+    else:
+        YearInterval = str(now.year) + '/' + str(now.year+1)
+    return YearInterval
+
+parseICS()
+importToGoogleCalendar()
