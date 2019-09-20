96957bfe9d818e000462e9fbd4a240489c122601
simonsanvil <simonsanvil2012@gmail.com>
2019-09-20 12:06:49 +0200

    Updated to handle request and login errors.

--- HorarioDeClases_app.py
+++ HorarioDeClases_app.py
@@ -14,14 +14,12 @@ def getHorarios():
     print('To obtain class calendars, UC3M Aula global credentials are required')
     URL = "https://aplicaciones.uc3m.es/horarios-web/alumno/alumno.page"
     #Aula Global Credentials:
-    if not os.path.isfile("AulaCredentials.txt"): #If it's the first time the app is used we dont have their credentials
+    if not os.path.isfile("AulaCredentials.txt"): #If we we dont have the credentials in a file already
         credentials = getCredentials() # TODO: NUMERO DE ESTUDIANTE y contrasena de aula global
         NIA = credentials[0]
         PASS = credentials[1]
-        with open("AulaCredentials.txt", mode = 'w') as f:
-            f.write(NIA + "\n")
-            f.write(PASS + "\n")
-    else: #Reading credentials from file in directory
+
+    else: #Reading credentials from file in directory if it exists
         with open("AulaCredentials.txt") as f:
             lineList = [line.rstrip('\n') for line in open("AulaCredentials.txt")]
             NIA = lineList[0]
@@ -36,27 +34,35 @@ def getHorarios():
                       "adAS_mode":"authn",
                       "adAS_username":NIA,
                       "adAS_password":PASS}
-        s.post("https://login.uc3m.es/index.php/CAS/login?service=https%3A%2F%2Faulaglobal.uc3m.es%2Flogin%2Findex.php&gateway=true",login_data)
-        print('Log in was succesful')
+        r = s.post("https://login.uc3m.es/index.php/CAS/login?service=https%3A%2F%2Faulaglobal.uc3m.es%2Flogin%2Findex.php&gateway=true",login_data)
         ##To enter the horarios webpage and access all the references with links
         print('Scraping ' + URL + ' to get .ics file')
-        calendar_page = s.get(URL)
+        try:
+            calendar_page = s.get(URL)
+        except:
+            print('There was  an issue trying to request the website. The site might be temporarilly inactive or the server doesnt respond')
+
         webpage = html.fromstring(calendar_page.content)
         hrefList = webpage.xpath('//a/@href')
         ##To download the ics files and put them into the "horarios" folder:
-        ## HORARIO CORRESPONDIENTE AL PRIMER CUATRIMESTRE:
-        calendarLink_1 = "https://aplicaciones.uc3m.es" + hrefList[2]
-        cal_q1 = s.get(calendarLink_1)
-        calFile_1 = str(os.path.dirname(os.path.realpath(__file__))) + "/horario_Q1.ics"
-        horarioCuatri_1 = open(calFile_1, 'wb').write(cal_q1.content)
-        print('Horario 1er Cuat. obtained')
+        try:
+            ## HORARIO CORRESPONDIENTE AL PRIMER CUATRIMESTRE:
+            calendarLink_1 = "https://aplicaciones.uc3m.es" + hrefList[2]
+            cal_q1 = s.get(calendarLink_1)
+            calFile_1 = str(os.path.dirname(os.path.realpath(__file__))) + "/horario_Q1.ics"
+            horarioCuatri_1 = open(calFile_1, 'wb').write(cal_q1.content)
 
-        ## HORARIO CORRESPONDIENTE AL SEGUNDO CUATRIMESTRE:
-        calendarLink_2 = "https://aplicaciones.uc3m.es" + hrefList[4]
-        cal_q2 = s.get(calendarLink_2)
-        calFile_2 = str(os.path.dirname(os.path.realpath(__file__))) + "/horario_Q2.ics"
-        horarioCuatri_2 = open(calFile_2, 'wb').write(cal_q2.content)
-        print('Horario 2do Cuat. obtained')
+            ## HORARIO CORRESPONDIENTE AL SEGUNDO CUATRIMESTRE:
+            calendarLink_2 = "https://aplicaciones.uc3m.es" + hrefList[4]
+            cal_q2 = s.get(calendarLink_2)
+            calFile_2 = str(os.path.dirname(os.path.realpath(__file__))) + "/horario_Q2.ics"
+            horarioCuatri_2 = open(calFile_2, 'wb').write(cal_q2.content)
+
+            print('Horario 1er Cuat. obtained')
+            print('Horario 2do Cuat. obtained')
+        except:
+            print("Failed to obtain the calendars from Aula Global. Try again and make sure your aula global username and password are correct.")
+
 
 def getCredentials():
     root = Tk()
@@ -93,6 +99,6 @@ def getCredentials():
 def installRequirementsWithPip():
         subprocess.call([sys.executable, "-m", "pip", "install", "requirements.txt"])
 
-installRequirementsWithPip()
+#installRequirementsWithPip()
 getHorarios()
-import ParsingAndImport_cal
+#import ParsingAndImport_cal
